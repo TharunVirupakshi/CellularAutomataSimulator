10c8ac115fdfc75ee023d6b3a53b0b79495fc9bb
TharunVirupakshi <tharunvrrvc184@gmail.com>
2024-02-05 19:52:59 +0530

    Simulation functionality added

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -23,7 +23,7 @@ const Cell = ({content, width, classes, onClickFunction}) => {
 
   return(
     <div className={'cell ' + classes} style={cellStyle} onClick={handleClick}>
-      {/* {content} */}
+      {content}
     </div>
   )
 }
@@ -35,7 +35,8 @@ const Player = () => {
   const [gridSize, setGridSize] = useState(2);
   const [gap, setGap] = useState(0);
   const [cellWidth, setCellWidth] = useState(50);
-  
+  const states = [0,1];
+  const TOT_STATES = states.length;
 
   const screenRef = useRef(null)
 
@@ -48,7 +49,7 @@ const Player = () => {
   // Function to initialize the grid with random alive and dead cells
   const initializeGrid = () => {
     return Array.from({ length: gridSize }, () =>
-      Array.from({ length: gridSize }, () => false) 
+      Array.from({ length: gridSize }, () => 0) 
     );
   };
   const [grid, setGrid] = useState(()=>initializeGrid());
@@ -77,8 +78,14 @@ const Player = () => {
     console.log('Grid height: ',screenRef.current.clientHeight)
   },)
   
-
   
+
+  const toggleState = (state, states) => {
+    const index = ((states.indexOf(state)) + 1) % states.length;
+    // console.log('ToggleState states: ', states, ' index: ', index);
+    
+    return states[index]
+  }
   //When you use the functional form of setGrid, 
   //React may batch multiple state updates together, and this can 
   //result in unexpected behavior when you toggle the cell state. 
@@ -100,14 +107,13 @@ const Player = () => {
   //   });
   // };
 
-  const toggleCell = (row, col, e) => {
-
-  
-    console.log('ToggleCell Triggered:', row, col);
-    
+  const toggleCell = (row, col) => {
+    // console.log('ToggleCell Triggered:', row, col);
     const newGrid = grid.map((rowArray, rowIndex) =>
-      rowArray.map((cell, colIndex) =>
-        rowIndex === row && colIndex === col ? !cell : cell
+      rowArray.map((cell, colIndex) =>{
+        // console.log('CurState: ', cell, ' NewState: ', toggleState(cell, states));
+        return rowIndex === row && colIndex === col ? toggleState(cell, states) : cell
+      }
       )
     );
     setGrid(newGrid);
@@ -126,6 +132,68 @@ const Player = () => {
       </div>
 
     )))
+  
+  //Calculate Neighbors LOGIC
+
+  const countNgbhrs = (grid,nbors, r0, c0) => {
+    // nbors.fill(0)
+    for(let r1=-1; r1<=1; ++r1){
+      for(let c1=-1; c1<= 1; ++c1){
+        if(r1!=0 || c1!=0){
+          const r = r0 + r1;
+          const c = c0 + c1;
+
+          if(r >= 0 && r < gridSize && c >= 0 && c < gridSize){
+            nbors[grid[r][c]]++
+            // console.log('nbrs counted for ',r0,' ',c0, ' is ', nbors);
+          }
+        }
+      }
+    }
+  }
+
+  //Compute Next Boards LOGIC
+  const computeNextGen = (states) => {
+    const DEAD = 0;
+    const ALIVE = 1;
+    const nbors = new Array(states).fill(0);
+
+    console.log('Computing next gen......');
+    console.log('nbors[]: ', nbors);
+
+    const nextGrid = grid.map((rowArray, row) =>
+    rowArray.map((cell, col) => {
+        nbors.fill(0); //init
+        countNgbhrs(grid, nbors, row, col)
+        console.log('nbrs counted for ',row,' ',col, ' is ', nbors);
+        switch (cell) {
+          case DEAD:
+            if(nbors[ALIVE] == 3) 
+              return ALIVE
+            else
+              return DEAD 
+            break;
+          
+          case ALIVE:
+            if(nbors[ALIVE] == 2 || nbors[ALIVE] == 3)
+              return ALIVE
+            else
+              return DEAD     
+        
+          default:
+            break;
+        }
+    }
+    )
+
+    );
+
+    console.log('NEXT GEN: ', nextGrid)
+
+    setGrid(nextGrid)
+
+  }
+
   
     
   
@@ -160,6 +228,8 @@ const Player = () => {
         <input type="text" onKeyDown={e => handleIntInputOnEnter(e, setGridSize)}/>
         <label> Cell width</label>
         <input type="text" onKeyDown={e => handleIntInputOnEnter(e, setCellWidth)}/>
+        
+        <button onClick={() => computeNextGen(TOT_STATES)} id='next-gen' style={{marginLeft: '20px'}}>NEXT GEN</button>
     </div>
   )
 }
